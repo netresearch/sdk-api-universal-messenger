b5a79d62d729a5a3d9c7cb14076b7aa0be3a9a06
Rico Sonntag <rico.sonntag@netresearch.de>
2025-02-12 14:35:36 +0000

    Update dev tools configuration

--- rector.php
+++ rector.php
@@ -11,18 +11,14 @@ declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedConstructorParamRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
 use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
 use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
-use Rector\DeadCode\Rector\Stmt\RemoveUnreachableStatementRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -41,40 +37,26 @@ return static function (RectorConfig $rectorConfig): void {
 
     // Define what rule sets will be applied
     $rectorConfig->sets([
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-        SetList::CODING_STYLE,
         SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
         SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+        SetList::PRIVATIZATION,
+        SetList::STRICT_BOOLEANS,
+        SetList::TYPE_DECLARATION,
+
         LevelSetList::UP_TO_PHP_82,
     ]);
 
     // Skip some rules
     $rectorConfig->skip([
         CatchExceptionNameMatchingTypeRector::class,
-        RemoveUnreachableStatementRector::class,
-        RemoveUnusedConstructorParamRector::class,
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        RemoveUnusedPrivateMethodParameterRector::class,
         RemoveUnusedPrivatePropertyRector::class,
         RemoveUselessParamTagRector::class,
         RemoveUselessReturnTagRector::class,
         RemoveUselessVarTagRector::class,
-        TypedPropertyFromAssignsRector::class,
-        TypedPropertyFromStrictConstructorRector::class,
     ]);
-
-    // PHP 8.0 dependent rules
-    if (version_compare(PHP_VERSION, '8.0.0') >= 0) {
-        // Skip some rules
-        $rectorConfig->skip([
-            ClassPropertyAssignToConstructorPromotionRector::class,
-        ]);
-    }
-
-    // PHP 8.1 dependent rules
-    if (version_compare(PHP_VERSION, '8.1.0') >= 0) {
-        // Skip some rules
-        $rectorConfig->skip([
-            NullToStrictStringFuncCallArgRector::class,
-        ]);
-    }
 };
